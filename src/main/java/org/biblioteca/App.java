8f0f65395878504efaa24b7e38f2301fd3753c51
Hansee <hansee1996@gmail.com>
2023-07-09 12:34:20 -0500

    Se corrije texto en main App

--- src/main/java/org/biblioteca/App.java
+++ src/main/java/org/biblioteca/App.java
@@ -16,8 +16,9 @@ import java.util.Scanner;
 /**
  * Clase principal para la ejecucion del programa del proyecto de Biblioteca para poder realizar operaciones (CRUD)
  * en tres tablas determinadas: Categorias, Editoriales y Libros
- * @author: Hansee Jimenez Perez - Sena
+ *
  * @version : 1.1
+ * @author: Hansee Jimenez Perez - Sena
  */
 public class App {
 
@@ -26,29 +27,35 @@ public class App {
     public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         String option;
         System.out.println("Bienvenido");
-        System.out.println("Por favor digite el numero de la tabla en la que desea realizar operaciones:");
+        while (true) {
+            showAvailableEntities();
+            String selection = input.nextLine();
+            switch (selection) {
+                case "1":
+                    CategoriasController categoriasController = new CategoriasController();
+                    option = categoriasController.selectAvailableOperations(new CategoriasRepositoryImp());
+                    categoriasController.executeOperationHandler(option, new CategoriasModel());
+                    return;
+                case "2":
+                    EditorialController editorialController = new EditorialController();
+                    option = editorialController.selectAvailableOperations(new EditorialesRepositoryImp());
+                    editorialController.executeOperationHandler(option, new EditorialesModel());
+                    return;
+                case "3":
+                    LibrosController librosController = new LibrosController();
+                    option = librosController.selectAvailableOperations(new LibrosRepositoryImp());
+                    librosController.executeOperationHandler(option, new LibrosModel());
+                    return;
+                default:
+                    System.out.println("Por favor elija uno de los numeros indicados.");
+            }
+        }
+    }
+
+    private static void showAvailableEntities() {
+        System.out.println("Digite el numero de la entidad en la que desea realizar operaciones:");
         System.out.println("1 => Categorias");
         System.out.println("2 => Editoriales");
         System.out.println("3 => Libros");
-        String selection = input.nextLine();
-        switch (selection) {
-            case "1":
-                CategoriasController categoriasController = new CategoriasController();
-                option = categoriasController.selectAvailableOperations(new CategoriasRepositoryImp());
-                categoriasController.executeOperationHandler(option, new CategoriasModel());
-                return;
-            case "2":
-                EditorialController editorialController = new EditorialController();
-                option = editorialController.selectAvailableOperations(new EditorialesRepositoryImp());
-                editorialController.executeOperationHandler(option, new EditorialesModel());
-                return;
-            case "3":
-                LibrosController librosController = new LibrosController();
-                option = librosController.selectAvailableOperations(new LibrosRepositoryImp());
-                librosController.executeOperationHandler(option, new LibrosModel());
-                return;
-            default:
-                System.out.println("Por favor elija uno de los numeros indicados.");
-        }
     }
 }
